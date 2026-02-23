eef7129e46e9c9bf09444f37714de6f2c006f030
gowtham72409 <mgowtham242002@gmail.com>
2026-02-23 20:53:46 +0530

    Add files via upload

---
+++ entrans.py
@@ -0,0 +1,19 @@
+# find all occurrences of the given character in the string and
+
+# str=input("Enter the sting:")
+# chr=input("Enter the character:")
+# c=[]
+# for i in range(1,len(str)):
+#     if chr==str[i]:
+#         c.append(str[i-1])
+# print(",".join(c))
+
+# a list of integers and a target number
+
+# list=[2,4,3,5,7,8,1]
+# target=9
+# for i in range(len(list)):
+#     for j in range(i+1,len(list)):
+#         if list[i]+list[j]==9:
+#             print((list[i],list[j]))
+
